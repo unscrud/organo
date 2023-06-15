796dd8eddfc5bb988a5b30af7e60cacd5baaae59
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-15 20:59:40 -0300

    validando campos vazios e captura de ação do btn

--- src/components/formulario/index.js
+++ src/components/formulario/index.js
@@ -9,13 +9,18 @@ const Formulario = () => {
     'Devops', 'UX e Design', 'Mobile', 'Inovação e Gestão'
   ]
 
+  const aoSalvar = (evento) => {
+    evento.preventDefault()
+    console.log('foi')
+  }
+
   return (
     <section className='formulario'>
-      <form action="">
+      <form onSubmit={aoSalvar}>
         <h2>Preencha para criar o card do colaborador</h2>
-        <CampoTexto label='Nome' placeholder='Digite o seu nome' />
-        <CampoTexto label='Cargo' placeholder='Digite o seu cargo' />
-        <CampoTexto label='Imagem' placeholder='Digite o endereço da imagem' />
+        <CampoTexto obrigatorio={true} label='Nome' placeholder='Digite o seu nome' />
+        <CampoTexto obrigatorio={true} label='Cargo' placeholder='Digite o seu cargo' />
+        <CampoTexto obrigatorio={false} label='Imagem' placeholder='Digite o endereço da imagem' />
         <ListaSuspensa label='Time' itens={times} />
         <Botao>
           Criar Card
