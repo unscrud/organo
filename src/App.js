c740a65827d5adc8dae9af42f36e570a3d92fa54
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-17 19:37:05 -0300

    controlando estado da lista de colaboradores

--- src/App.js
+++ src/App.js
@@ -1,11 +1,18 @@
+import { useState } from 'react';
 import Banner from './components/banner';
 import Formulario from './components/formulario';
 
 function App() {
+  const [colaboradores, setColaboradores] = useState([])
+
+  const aoNovoColaboradorAdicionado = (colaborador) => {
+    setColaboradores([...colaboradores, colaborador])
+  }
+
   return (
     <div className="App">
       <Banner />
-      <Formulario />
+      <Formulario aoColaboradorCadastrado={colaborador => aoNovoColaboradorAdicionado(colaborador)} />
     </div>
   );
 }
