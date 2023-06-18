04606d803fe8f988de1dc470b25b5c2bb1e76f2b
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-18 17:07:18 -0300

    colocando cards lado a lado

--- src/components/time/index.js
+++ src/components/time/index.js
@@ -5,15 +5,17 @@ const Time = (props) => {
   return (
     <section className='time' style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-      {
-        props.colaboradores.map((colaborador, index) => <Colaborador
-          nome={colaborador.nome}
-          key={colaborador.nome + index}
-          cargo={colaborador.cargo}
-          imagem={colaborador.imagem}
-          time={colaborador.time}
-        />)
-      }
+      <div className="colaboradores">
+        {
+          props.colaboradores.map((colaborador, index) => <Colaborador
+            nome={colaborador.nome}
+            key={colaborador.nome + index}
+            cargo={colaborador.cargo}
+            imagem={colaborador.imagem}
+            time={colaborador.time}
+          />)
+        }
+      </div>
     </section>
   )
 }
