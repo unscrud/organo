436ad311c5a7d02ffd324231868b8ea89f8e716c
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-18 16:56:53 -0300

    criação dos colaboradores

--- src/components/time/index.js
+++ src/components/time/index.js
@@ -1,13 +1,19 @@
 import Colaborador from '../colaborador'
 import './Time.css'
 
-
 const Time = (props) => {
   return (
     <section className='time' style={{ backgroundColor: props.corSecundaria }}>
       <h3 style={{ borderColor: props.corPrimaria }}>{props.nome}</h3>
-      <Colaborador />
-      <Colaborador />
+      {
+        props.colaboradores.map((colaborador, index) => <Colaborador
+          nome={colaborador.nome}
+          key={colaborador.nome + index}
+          cargo={colaborador.cargo}
+          imagem={colaborador.imagem}
+          time={colaborador.time}
+        />)
+      }
     </section>
   )
 }
