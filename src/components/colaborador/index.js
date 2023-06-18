797ffe08e0b115547afb316c1f6bc13cb21a51f2
Daniel Carvalho <daniel.devop@gmail.com>
2023-06-18 15:56:39 -0300

    componente do card de colaborador

---
+++ src/components/colaborador/index.js
@@ -0,0 +1,17 @@
+import './Colaborador.css'
+
+const Colaborador = () => {
+  return (
+    <div className='colaborador'>
+      <div className='cabecalho'>
+        <img src='https://github.com/unscrud.png' alt='UnsCrud Dev Avatar' />
+      </div>
+      <div className='rodape'>
+        <h4>UnsCrud Dev</h4>
+        <h5>Desenvolvedor</h5>
+      </div>
+    </div>
+  )
+}
+
+export default Colaborador
